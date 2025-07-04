91fa81fde3d2a705ab03d54396ad5c565603736d
Prastabm <prastabm@gmail.com>
2025-07-04 11:16:10 +0530

    product service setup

---
+++ product-service/src/test/java/com/clotho_microservices/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clotho_microservices.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
